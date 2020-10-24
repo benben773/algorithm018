9bf630a30e5346c53566d40a7d1a224533188dea
lis05 <lishuang_read@126.com>
2020-10-25 03:45:23 +0800

    接雨水

---
+++ week01/[42]接雨水.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+//
+// 
+//
+// 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Mar
+//cos 贡献此图。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+//输出: 6 
+// Related Topics 栈 数组 双指针 
+// 👍 1751 👎 0
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class 接雨水 {
+    public static void main(String[] args) {
+        接雨水 接雨水 = new 接雨水();
+        int trap = 接雨水.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+        System.out.println(trap);
+    }
+    public int trap(int[] height) {
+
+        int result = 0, current = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        while (current < height.length) {
+            while (!stack.isEmpty() && height[current] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty())
+                    break;
+                int distance = current - stack.peek() - 1;
+                int bounded_height = Math.min(height[current], height[stack.peek()]) - height[top];
+                result += distance * bounded_height;
+            }
+            stack.push(current++);
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
